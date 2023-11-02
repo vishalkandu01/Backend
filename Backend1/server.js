c1b8f1f788bc0d784f8508f9b3a1a40cb198924d
Vishal Kandu <kanduvishal07@gmail.com>
2023-11-02 16:59:59 +0530

    get, post and body-parser is done

--- Backend1/server.js
+++ Backend1/server.js
@@ -1,6 +1,26 @@
-const express = require('exrpess');
+// server initiate
+const express = require('express');
 const app = express();
 
-app.listen(300, () => {
-    console.log("Server started at port no. 3000");
+//used to parse req.body in express -> PUT or POST
+const bodyParser = require('body-parser');
+
+//specifically parse JSON data & add it to the request.Body object
+app.use(bodyParser.json());
+
+// activate the server on port 3000
+app.listen(3000, () => {
+    console.log("Server is listening on port no. 3000");
+});
+
+// Routes 
+app.get('/', (req, res) => {
+    res.send("hello, jee, kaise ho saare");
+})
+
+app.post('/api/cars', (req, res) => {
+    const {name, brand} = req.body;
+    console.log(name);
+    console.log(brand);
+    res.send("Car submitted successfully.");
 })
